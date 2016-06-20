b930c6e0d00119472b56ba314b326ba17e9d5390
nik frank <nikbfrank@gmail.com>
2016-06-20 20:50:56 +0300

    clean out dep code

--- app.js
+++ app.js
@@ -32,44 +32,7 @@ io.on('connection', function(socket){
   socket.on('answer-to-patron', ({answer, patronId})=>{
     io.to(patronId).emit('answer-to-offerer', answer);
   });
-  
-  socket.on('icecandidate', data=>{
-    if(data.room in rooms) return;
-
-    data.hostId = socket.id;
-    rooms[data.room] = data;
-    
-    console.log(socket.id, data, rooms);
-    socket.broadcast.emit('icecandidate', rooms[data.room].candidate);
-  });
-
-
-  socket.on('offer', data=>{
-    console.log('offer', socket.id, data);
-    if(data.room in offers) return;
-      
-    else{
-      offers[data.room] = data;
-
-
-      socket.broadcast
-      //      io.to(rooms[data.room].hostId)
-	.emit('offer', offers[data.room]);
-    }
-  });
-
-  socket.on('answer', data=>{
-    console.log('ans', data);
-    if(data.room in answers) return;
-
-    answers[data.room] = data.answer;
-
-    socket.broadcast.emit('answer', answers[data.room]);
-  });
 });
-
-
-
 
 
 
